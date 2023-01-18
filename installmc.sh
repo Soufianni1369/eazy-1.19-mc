c3814f90fad0f0feadd78edd2e4d4e00a32d76c9
Soufian Smyej <91152489+Soufianni1369@users.noreply.github.com>
2023-01-18 15:35:46 +0100

    Add files via upload

---
+++ installmc.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade
+apt install sudo
+sudo apt update
+sudo apt upgrade
+sudo apt install openjdk-17-jdk openjdk-17-jre
+sudo apt install unzip
+wget http://files.ikwilsus.tk/utilities/mc/1.19.zip
+unzip 1.19.zip
+rm 1.19.zip
+rm installmc.sh
+wget http://files.ikwilsus.tk/utilities/mc/rmmc.sh
+cd 1.19
+bash ./start.sh
